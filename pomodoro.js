ecd7b95db9115586e9300b744923fe9da3c6fbfc
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-15 16:45:44 -0400

    Added event handlers for the start, stop, and puase buttons

--- pomodoro.js
+++ pomodoro.js
@@ -2,8 +2,62 @@ $(document).ready(function() {
     var break_minus = $(".breakminus");
     var break_time = $(".breaktime");
     var break_plus = $(".breakadd");
-
-    var session_minus = $(".sessionplus");
+    var session_minus = $(".sessionminus");
     var session_time = $(".sessiontime");
     var session_plus = $(".sessionadd");
+    var start = $(".start");
+    var pause = $(".pause");
+    var stop = $(".stop");
+    var facetime = $(".facetime");
+
+    //Handle the plus/minus buttons for session and break settings
+
+    break_minus.click(function(){
+        var current_break_length = break_time.html();
+        if(current_break_length > 0) {
+            break_time.html(Number(current_break_length) -1);
+        }
+    });
+    break_plus.click(function(){
+        var current_break_length = break_time.html();
+        break_time.html(Number(current_break_length)+1);
+    });
+    session_minus.click(function(){
+        var current_session_length = session_time.html();
+        if(current_session_length > 0) {
+            session_time.html(Number(current_session_length) -1);
+        }
+    });
+    session_plus.click(function(){
+        var current_session_length = session_time.html();
+        session_time.html(Number(current_session_length)+1);
+    });    
+
+    //Handle the start button
+    start.click(function(){
+        start.prop("disabled", "true");
+        start.css("background-color", "grey");
+        break_minus.hide();
+        break_plus.hide();
+        session_plus.hide();
+        session_minus.hide();
+    });
+
+    //handle pause button
+    var boolean = true;
+    pause.click(function() {
+        if(boolean) {
+            boolean = false;
+        } else {
+            boolean = true;
+        }
+
+    });
+
+    //handle stop
+
+
+
+
+
 });
