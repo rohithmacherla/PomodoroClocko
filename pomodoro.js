6edf08b529b8c1130b6f62577354afe94096f7b0
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-15 16:20:26 -0400

    Connected the buttons to jquery objects in javascript, added more styles

---
+++ pomodoro.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var break_minus = $(".breakminus");
+    var break_time = $(".breaktime");
+    var break_plus = $(".breakadd");
+
+    var session_minus = $(".sessionplus");
+    var session_time = $(".sessiontime");
+    var session_plus = $(".sessionadd");
+});
